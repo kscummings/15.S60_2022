eac682ea6e81d595c6fc45060aa7d0de1c64b879
KaylaCummings47 <ksc72014@mymail.pomona.edu>
2022-01-20 01:38:14 -0500

    session 6 materials

--- 6_linear_programming/data/raw/raw_format.py
+++ 6_linear_programming/data/raw/raw_format.py
@@ -6,6 +6,9 @@ import string
 
 from shapely.geometry import shape
 
+LON_BD = (-71.3, -70.6)
+LAT_BD = (42, 42.75)
+
 # read raw data
 with open("ma_pop.geojson") as f:
     g=json.load(f)
@@ -24,6 +27,7 @@ df = df.rename(columns={"name":"zip_code", "B01003001":"population", "B01003001,
 
 # obtain interpretable ids for each zip code
 zc["zip_code"] = [z.replace("ZIP Code ", '') for z in zc["ZIP Code"]]
+zc = zc[(zc["ZIP Code"] != "01062") | ((zc["ZIP Code"] == "01062") & (zc["City"] == "Florence"))]
 df = df.merge(zc[["zip_code", "City", "County"]], on='zip_code', how='left')
 df = df.rename(columns={"City":"city", "County":"county"})
 
@@ -33,6 +37,9 @@ zip_code = df.zip_code
 df = df.drop("zip_code", axis=1)
 df["zip_code"] = ["z"+z for z in zip_code]
 # this is to fix an issue where zip codes are read in as Int types, resulting in 0's being removed from beginning/end of zip code
+
+# limit to GBA
+#df = df[(df.longitude >= LON_BD[0]) & (df.longitude <= LON_BD[1]) & (df.latitude >= LAT_BD[0]) & (df.latitude <= LAT_BD[1])]
 
 # write formatted dataframe
 df.to_csv(os.path.join("..", "formatted", "ma_population.csv"), index=False)
